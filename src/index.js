7387db0102473124ba71c759c5dffddc35a740a7
Matt Orrock <mattorrock@gmail.com>
2020-05-30 10:45:09 -0400

    added margins to date selector chevrons so that the user has to click on the actual icon to move to yesterday or tomorrow, rather than the entire container

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-
 import './index.css';
 import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+// serviceWorker.register();
 
 ReactDOM.render(
   <Provider store={store}>
